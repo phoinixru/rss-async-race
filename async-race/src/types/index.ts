4a311a9d8cb28056a46777859235b10eb20ded07
Vitaly Kukushkin <phoinixru@gmail.com>
2023-07-19 20:38:45 +0200

    feat: describe types

---
+++ async-race/src/types/index.ts
@@ -0,0 +1,55 @@
+export type Car = {
+  id: number;
+  name: string;
+  color: string;
+};
+
+export type Winner = {
+  id: number;
+  wins: number;
+  time: number;
+};
+
+export type Sort = 'id' | 'wins' | 'time';
+
+export type Order = 'ASC' | 'DESC';
+
+export enum Makes {
+  'Mercedes-Benz',
+  'Audi',
+  'GMC',
+  'Hyundai',
+  'Honda',
+  'Lucid',
+  'Nissan',
+  'Volkswagen',
+  'McLaren',
+  'Subaru',
+  'Toyota',
+  'Lamborghini',
+  'Lincoln',
+}
+
+export enum Models {
+  'Accord',
+  'Accent',
+  'Altima',
+  'Atlas',
+  'Blazer',
+  'BRZ',
+  'Carnival',
+  'Cayenne',
+  'Charger',
+  'CLE',
+  'Corolla',
+  'Corsair',
+  'Crown',
+  'CX-9',
+}
+
+export type ElementProps = {
+  className?: string;
+  innerHTML?: string;
+  href?: string;
+  placeholder?: string;
+} | null;
