d238d8ef09852365cd9c8e186df7148130af995b
Vitaly Kukushkin <phoinixru@gmail.com>
2023-07-20 17:42:38 +0200

    feat: add random color util

--- async-race/src/utils/index.ts
+++ async-race/src/utils/index.ts
@@ -25,5 +25,16 @@ function elt<T extends HTMLElement>(type: string, props?: ElementProps, ...child
   return dom;
 }
 
+const pause = (msec: number): Promise<void> =>
+  new Promise((resolve) => {
+    setTimeout(resolve, msec, `pause ${msec}`);
+  });
+
+const errorHandler = (error: Error): void => console.error(error);
+
+const randomByte = (): number => Math.floor(Math.random() * 256);
+const toHex = (number: number): string => number.toString(16).padStart(2, '0');
+const getRandomColor = (): string => `#${Array(3).fill(0).map(randomByte).map(toHex).join('')}`;
+
 const { keys, values, assign, entries, fromEntries } = Object;
-export { qs, qsa, elt, keys, values, assign, entries, fromEntries };
+export { qs, qsa, elt, keys, values, assign, entries, fromEntries, pause, errorHandler, getRandomColor };
